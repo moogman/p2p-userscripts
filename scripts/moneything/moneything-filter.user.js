5584223886cd2d0f25dc6df8673a2464f19b505b
moogman <moogman@gmail.com>
2017-06-28 17:03:52 +0100

    Bump version to 0.1.4

--- scripts/moneything/moneything-filter.user.js
+++ scripts/moneything/moneything-filter.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         moneything-filter
 // @namespace    https://github.com/moogman/p2p-userscripts/scripts/moneything
-// @version      0.1.3
+// @version      0.1.4
 // @description  Filter out various things on the MoneyThing website.
 // @author       moogman
 // @match        https://www.moneything.com/p2p/index.php/loan
